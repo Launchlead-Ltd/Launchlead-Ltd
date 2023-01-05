3fe2dc9cc1c803d94dd27f1c357c932c97fe5a63
Liam Devaney <7091410+Devaney@users.noreply.github.com>
2023-01-05 22:41:20 +0000

    feat: initial site layout and nav plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
     // Input Directory : src Output directory: _site
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
     return {
         dir: { input: 'src', output: '_site' }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./_site/**/*.{html,js}"],
+  content: [
+    "./_site/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+    "./src/_includes/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+    "./src/pages/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+    "./src/index.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+  ],
   theme: {
     extend: {},
   },
