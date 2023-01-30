d9f13b54ce5054076f582c3bbd9b1c693f722af1
Liam Devaney <7091410+Devaney@users.noreply.github.com>
2023-01-30 23:06:42 +0000

    Adding basic content

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
     // Input Directory : src Output directory: _site
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
-    eleventyConfig.addPassthroughCopy("src/img");
+    eleventyConfig.addPassthroughCopy("src/img/");
     // eleventyConfig.addPassthroughCopy("**/*.jpg");
     // eleventyConfig.addPassthroughCopy("**/*.gif");
 
